3066bbb6cad1426b1d13d9277bb7e0e0da171e21
Posttwo <posttwo@users.noreply.github.com>
2020-05-25 15:54:52 +0100

    Update punish.py

--- punish/punish.py
+++ punish/punish.py
@@ -36,7 +36,7 @@ class PunishCog(BaseCog):
     @commands.Cog.listener()
     async def on_member_update(self, before, after):
         print("Member updated")
-        member_data = await self.config.member(before).forced_nickname
+        member_data = self.config.member(before).forced_nickname
         if member_data == "":
             return
 
